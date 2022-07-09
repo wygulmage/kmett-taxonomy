73bc589a9712c5f1e5134e8a967becf16a0d998a
Keith <wygulmage@users.noreply.github.com>
2022-07-09 19:55:25 -0400

    changed Reader generics.

--- control-monad-reader-class/src/Control/Monad/Reader/Class.hs
+++ control-monad-reader-class/src/Control/Monad/Reader/Class.hs
@@ -24,9 +24,9 @@ class (Monad m)=> MonadReader i m | m -> i where
     reader :: (i -> a) -> m a
     local :: (i -> i) -> m a -> m a
 
-    default reader :: (G.Generic1 m, MonadReader i (G.Rep1 m))=> (i -> a) -> m a
+    default reader :: (G.Generic1 m, GReader i (G.Rep1 m))=> (i -> a) -> m a
     reader = greader
-    default local :: (G.Generic1 m, MonadReader i (G.Rep1 m))=> (i -> i) -> m a -> m a
+    default local :: (G.Generic1 m, GLocal i (G.Rep1 m))=> (i -> i) -> m a -> m a
     local = glocal
 
 ask :: (MonadReader i m)=> m i
@@ -52,21 +52,34 @@ instance (Monad m)=> MonadReader i (Star m i) where
 
 #if __GLASGOW_HASKELL__ && __GLASGOW_HASKELL__ >= 720
 --- Generics ---
-instance (MonadReader i m)=> MonadReader i (G.Rec1 m) where
-    reader f = G.Rec1 (reader f)
-    local = coerce (local :: (i -> i) -> m a -> m a) :: forall a. (i -> i) -> G.Rec1 m a -> G.Rec1 m a
+class GReader i m where greader' :: (i -> a) -> m a
+class GLocal i m where glocal' :: (i -> i) -> m a -> m a
 
-instance (MonadReader i m)=> MonadReader i (G.M1 j meta m) where
-    reader f = G.M1 (reader f)
-    local = coerce (local :: (i -> i) -> m a -> m a)
+instance (MonadReader i m)=> GReader i (G.Rec1 m) where
+    greader' = coerce (reader :: (i -> a) -> m a) :: forall a. (i -> a) -> G.Rec1 m a
+instance (MonadReader i m)=> GLocal i (G.Rec1 m) where
+    glocal' = coerce (local :: (i -> i) -> m a -> m a) :: forall a. (i -> i) -> G.Rec1 m a -> G.Rec1 m a
+
+instance (MonadReader i m)=> GReader i (G.M1 j meta m) where
+    greader' = coerce (reader :: (i -> a) -> m a) :: forall a. (i -> a) -> G.M1 j meta m a
+instance (MonadReader i m)=> GLocal i (G.M1 j meta m) where
+    glocal' = coerce (local :: (i -> i) -> m a -> m a)
         :: forall a. (i -> i) -> G.M1 j meta m a -> G.M1 j meta m a
 
-greader :: (G.Generic1 m, MonadReader i (G.Rep1 m))=> (i -> r) -> m r
-greader = G.to1 . reader
+instance (Applicative m)=> GReader i ((G.:.:) ((->) i) m) where
+    greader' f = G.Comp1 (pure . f)
+    {-# INLINE greader' #-}
+instance GLocal i ((G.:.:) ((->) i) m) where
+    glocal' f (G.Comp1 g) = G.Comp1 (g . f)
+    {-# INLINE glocal' #-}
+
+
+greader :: (G.Generic1 m, GReader i (G.Rep1 m))=> (i -> r) -> m r
+greader = G.to1 . greader'
 {-# INLINE greader #-}
 
-glocal :: (G.Generic1 m, MonadReader i (G.Rep1 m))=> (i -> i) -> m a -> m a
-glocal f = G.to1 . local f . G.from1
+glocal :: (G.Generic1 m, GLocal i (G.Rep1 m))=> (i -> i) -> m a -> m a
+glocal f = G.to1 . glocal' f . G.from1
 {-# INLINE glocal #-}
 
 #endif
