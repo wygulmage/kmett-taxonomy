1160fd7aed0c9850d2e3257aec432d407722e080
Keith <wygulmage@users.noreply.github.com>
2022-07-10 17:27:15 -0400

    corrected Strong Star constraint

--- data-profunctor-strong/src/Data/Profunctor/Strong.hs
+++ data-profunctor-strong/src/Data/Profunctor/Strong.hs
@@ -10,7 +10,7 @@ import Data.Profunctor.Types.Star
 class (Profunctor p)=> Strong p where
     second :: p a b -> p (c, a) (c, b)
 
-instance (Applicative m)=> Strong (Star m) where
+instance (Functor m)=> Strong (Star m) where
     second (Star k) = Star (\ (u, x) -> fmap ((,) u) (k x))
     {-# INLINABLE second #-}
 
