aaceb5b198ca5203ac5dab5e4f9b0ebdd41e696c
Keith <wygulmage@users.noreply.github.com>
2022-07-09 20:39:23 -0400

    simplified Distributive code

--- data-distributive/src/Data/Distributive.hs
+++ data-distributive/src/Data/Distributive.hs
@@ -26,9 +26,10 @@ class (Monad m)=> Distributive m where
     collect :: (Functor n)=> (a -> m b) -> n a -> m (n b)
 
     default collect ::
-        (g ~ G.Rep1 m, G.Generic1 m, Distributive g,  Functor n)=>
+        (g ~ G.Rep1 m, G.Generic1 m, Distributive g, Functor n)=>
         (a -> m b) -> n a -> m (n b)
     collect = gcollect
+    {-# INLINABLE collect #-}
 
 distribute :: (Distributive m, Functor n)=> n (m a) -> m (n a)
 distribute = collect id
@@ -39,24 +40,16 @@ instance Distributive ((->) c) where
     {-# INLINE collect #-}
 
 instance Distributive Identity where
-    collect = collectIdentity
-      where
-        collectIdentity ::
-            forall m a b. (Functor m)=> (a -> Identity b) -> m a -> Identity (m b)
-        collectIdentity = coerce (fmap :: (a -> b) -> m a -> m b)
-        {-# INLINE collectIdentity #-}
+    collect = coerce (fmap :: (a -> b) -> m a -> m b)
+        :: forall m a b. (Functor m)=> (a -> Identity b) -> m a -> Identity (m b)
     {-# INLINE collect #-}
 
 instance Distributive Proxy where
     collect _ _ = Proxy
 
 instance Distributive (Tagged t) where
-    collect = collectTagged
-      where
-        collectTagged ::
-            forall m a b. (Functor m)=> (a -> Tagged t b) -> m a -> Tagged t (m b)
-        collectTagged = coerce (fmap :: (a -> b) -> m a -> m b)
-        {-# INLINE collectTagged #-}
+    collect = coerce (fmap :: (a -> b) -> m a -> m b)
+        :: forall m a b. (Functor m)=> (a -> Tagged t b) -> m a -> Tagged t (m b)
     {-# INLINE collect #-}
 
 
@@ -66,31 +59,18 @@ instance Distributive G.U1 where
     collect _ _ = G.U1
 
 instance Distributive G.Par1 where
-    collect = collectPar1
-      where
-        collectPar1 ::
-            forall m a b. (Functor m)=> (a -> G.Par1 b) -> m a -> G.Par1 (m b)
-        collectPar1 = coerce (fmap :: (a -> b) -> m a -> m b)
-        {-# INLINE collectPar1 #-}
+    collect = coerce (fmap :: (a -> b) -> m a -> m b)
+      :: forall m a b. (Functor m)=> (a -> G.Par1 b) -> m a -> G.Par1 (m b)
     {-# INLINE collect #-}
 
 instance (Distributive m)=> Distributive (G.Rec1 m) where
-    collect = collectRec1
-      where
-        collectRec1 ::
-            forall n a b. (Functor n)=> (a -> G.Rec1 m b) -> n a -> G.Rec1 m (n b)
-        collectRec1 = coerce (collect :: (a -> m b) -> n a -> m (n b))
-        {-# INLINE collectRec1 #-}
+    collect = coerce (collect :: (a -> m b) -> n a -> m (n b))
+        :: forall n a b. (Functor n)=> (a -> G.Rec1 m b) -> n a -> G.Rec1 m (n b)
     {-# INLINE collect #-}
 
 instance (Distributive m)=> Distributive (G.M1 i meta m) where
-    collect = collectM1
-      where
-        collectM1 ::
-            forall n a b. (Functor n)=>
-            (a -> G.M1 i meta m b) -> n a -> G.M1 i meta m (n b)
-        collectM1 = coerce (collect :: (a -> m b) -> n a -> m (n b))
-        {-# INLINE collectM1 #-}
+    collect = coerce (collect :: (a -> m b) -> n a -> m (n b))
+        :: forall n a b. (Functor n)=> (a -> G.M1 i meta m b) -> n a -> G.M1 i meta m (n b)
     {-# INLINE collect #-}
 
 instance (Distributive m, Distributive n)=> Distributive ((G.:*:) m n) where
