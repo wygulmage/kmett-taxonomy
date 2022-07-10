3869f39804e500e1e2ef35b224032645643a96c5
Keith <wygulmage@users.noreply.github.com>
2022-07-10 17:26:53 -0400

    corrected name of Choice method

--- data-profunctor-choice/src/Data/Profunctor/Choice.hs
+++ data-profunctor-choice/src/Data/Profunctor/Choice.hs
@@ -4,13 +4,14 @@ module Data.Profunctor.Choice where
 
 
 import Data.Profunctor.Internal
+import Data.Profunctor.Types.Star
 import Data.Tagged
 
 
 class (Profunctor p)=> Choice p where
-    second :: p a b -> p (Either c a) (Either c b)
+    right' :: p a b -> p (Either c a) (Either c b)
 
 
-instance Choice (->) where second = fmap
+instance Choice (->) where right' = fmap
 
-instance Choice Tagged where second (Tagged x) = Tagged (Right x)
+instance Choice Tagged where right' (Tagged x) = Tagged (Right x)
